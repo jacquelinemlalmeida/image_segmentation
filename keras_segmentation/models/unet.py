6345babf53cc0dea05e194644f51edf8ca3a126d
jacquelinemlalmeida <58194876+jacquelinemlalmeida@users.noreply.github.com>
2020-10-30 18:16:23 -0300

    Update unet.py
    Alterando filtros da unet mini

--- keras_segmentation/models/unet.py
+++ keras_segmentation/models/unet.py
@@ -22,40 +22,40 @@ def unet_mini(n_classes, input_height=360, input_width=480):
     elif IMAGE_ORDERING == 'channels_last':
         img_input = Input(shape=(input_height, input_width, 3))
 
-    conv1 = Conv2D(32, (3, 3), data_format=IMAGE_ORDERING,
+    conv1 = Conv2D(16, (3, 3), data_format=IMAGE_ORDERING,
                    activation='relu', padding='same')(img_input)
     conv1 = Dropout(0.2)(conv1)
-    conv1 = Conv2D(32, (3, 3), data_format=IMAGE_ORDERING,
+    conv1 = Conv2D(16, (3, 3), data_format=IMAGE_ORDERING,
                    activation='relu', padding='same')(conv1)
     pool1 = MaxPooling2D((2, 2), data_format=IMAGE_ORDERING)(conv1)
 
-    conv2 = Conv2D(64, (3, 3), data_format=IMAGE_ORDERING,
+    conv2 = Conv2D(32, (3, 3), data_format=IMAGE_ORDERING,
                    activation='relu', padding='same')(pool1)
     conv2 = Dropout(0.2)(conv2)
-    conv2 = Conv2D(64, (3, 3), data_format=IMAGE_ORDERING,
+    conv2 = Conv2D(32, (3, 3), data_format=IMAGE_ORDERING,
                    activation='relu', padding='same')(conv2)
     pool2 = MaxPooling2D((2, 2), data_format=IMAGE_ORDERING)(conv2)
 
-    conv3 = Conv2D(128, (3, 3), data_format=IMAGE_ORDERING,
+    conv3 = Conv2D(64, (3, 3), data_format=IMAGE_ORDERING,
                    activation='relu', padding='same')(pool2)
     conv3 = Dropout(0.2)(conv3)
-    conv3 = Conv2D(128, (3, 3), data_format=IMAGE_ORDERING,
+    conv3 = Conv2D(64, (3, 3), data_format=IMAGE_ORDERING,
                    activation='relu', padding='same')(conv3)
 
     up1 = concatenate([UpSampling2D((2, 2), data_format=IMAGE_ORDERING)(
         conv3), conv2], axis=MERGE_AXIS)
-    conv4 = Conv2D(64, (3, 3), data_format=IMAGE_ORDERING,
+    conv4 = Conv2D(32, (3, 3), data_format=IMAGE_ORDERING,
                    activation='relu', padding='same')(up1)
     conv4 = Dropout(0.2)(conv4)
-    conv4 = Conv2D(64, (3, 3), data_format=IMAGE_ORDERING,
+    conv4 = Conv2D(32, (3, 3), data_format=IMAGE_ORDERING,
                    activation='relu', padding='same')(conv4)
 
     up2 = concatenate([UpSampling2D((2, 2), data_format=IMAGE_ORDERING)(
         conv4), conv1], axis=MERGE_AXIS)
-    conv5 = Conv2D(32, (3, 3), data_format=IMAGE_ORDERING,
+    conv5 = Conv2D(16, (3, 3), data_format=IMAGE_ORDERING,
                    activation='relu', padding='same')(up2)
     conv5 = Dropout(0.2)(conv5)
-    conv5 = Conv2D(32, (3, 3), data_format=IMAGE_ORDERING,
+    conv5 = Conv2D(16, (3, 3), data_format=IMAGE_ORDERING,
                    activation='relu', padding='same')(conv5)
 
     o = Conv2D(n_classes, (1, 1), data_format=IMAGE_ORDERING,
